76a1116d57321cfe1e64ac2c6b8664ee438b5edb
Walter Aab <aabwalter@yahoo.com>
2021-12-04 19:58:48 -0600

    Add new counter to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Counter from './Counter';
+import Newcounter from './NewCounter';
 
 function App() {
   return (
     <div className="App">
+      <div>
       <Counter />
+      </div>
+      <div>
+      <Newcounter />
+      </div>
     </div>
   );
 }
